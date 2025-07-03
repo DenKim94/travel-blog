cb1f6762fdc80d48c515897f6ad1036c97565647
Denis Kim <denis-kim@outlook.de>
2025-07-03 20:46:38 +0200

    Added documentation

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,17 @@ import * as appConstants from "@utils/appConstants"
 import * as helperFunctions from "@utils/helperFunctions"
 
 
+/**
+ * Middleware-Funktion zur Verarbeitung eingehender Anfragen.
+ *
+ * - Ignoriert Anfragen an bestimmte Pfade wie `/api`, `/_next`, `/assets`, `/favicon.ico`, und `/.well-known`.
+ * - Überprüft, ob das erste Segment des Pfades eine unterstützte Sprache ist.
+ *   - Wenn keine Sprache im Pfad enthalten ist, wird auf die Standardsprache umgeleitet.
+ *   - Wenn eine unterstützte Sprache im Pfad enthalten ist, wird die Anfrage normal weitergeleitet.
+ *
+ * @param request - Das eingehende NextRequest-Objekt, das Informationen über die Anfrage enthält.
+ * @returns NextResponse - Die Antwort, die entweder die Anfrage weiterleitet oder umleitet.
+ */
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
