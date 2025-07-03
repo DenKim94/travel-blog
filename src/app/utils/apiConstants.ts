bdc81bdb358faf70b29248258d6090a88ea4ceac
Denis Kim <denis-kim@outlook.de>
2025-07-03 20:46:17 +0200

    Added default value for page pagination

--- src/app/utils/apiConstants.ts
+++ src/app/utils/apiConstants.ts
@@ -2,6 +2,7 @@ export const REVALIDATION_TIME_BLOG_POSTS = 60 * 30;          // 30 minutes in s
 export const REVALIDATION_TIME_ABOUT_PAGE = 60 * 60 * 24;     // 1 day in seconds
 export const REVALIDATION_TIME_LANDING_PAGE = 60 * 60 * 24;   // 1 day in seconds
 export const REVALIDATION_TIME_GENERIC = 60 * 60;  // 1 hour in seconds
+export const MAX_PAGE_SIZE_DEFAULT = 20;  // Maximum number of items per page for pagination
 
 export const STRAPI_BLOGPOSTS_ENDPOINT = '/api/blog-posts';
 export const STRAPI_ABOUT_ENDPOINT = '/api/about-page';
